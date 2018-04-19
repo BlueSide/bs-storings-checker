c988a277f6bac8af2e7fc56d374d9bc2826df9f8
trajectcontrole <thomas@blueside.nl>
2018-04-20 00:13:07 +0200

    Textual changes

--- public/partials/bs-storings-checker-public-display.php
+++ public/partials/bs-storings-checker-public-display.php
@@ -16,12 +16,12 @@
 <div class="bs-storings-form">
         <div id="sc-error" style="display:none">Geen geldige postcode!</div>
     <div id="sc-not-found" style="display:none">De postcode is niet gevonden!</div>
-    <div id="sc-found" style="display:none">Er zijn geen storingen bekend in uw postcodegebied.</div>
+    <div id="sc-found" style="display:none">Wij zijn niet op de hoogte van een storing of werkzaamheden op uw adres. Het is belangrijk om te weten waar de storing zich bevindt: in onze installatie of in uw binnenhuisinstallatie. <a href="/veelgestelde-vragen/">Waar zit de storing.</a></div>
     <div uk-spinner id="sc-spinner"  style="display:none"></div>
     <div class="bs-form-div">
         <form class="uk-grid-small">
                 <div class="uk-width-1-2">
-                    <input type="text" id="sc-postcode" class="uk-input" placeholder="Postcode"/>
+                    <input type="text" id="sc-postcode" class="uk-input" placeholder="Zoek uw postcode, bijvoorbeeld 1234AA"/>
                 </div>
                 <div class="uk-width-1-2">
                     <button type="button" class="check-storing uk-button uk-button-primary">Check</button>
